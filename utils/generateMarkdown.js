9c7fa2c86894b8d35a0cf32dceb47932e251bf92
Ana Ea <aabad0464@gmail.com>
2021-06-22 23:20:45 -0600

    added function to render license badge & link to Readme file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -44,24 +44,23 @@ function renderLicenseLink(license) {
     case 'Boost Software License 1.0':
       badgeLic = "://www.boost.org/LICENSE_1_0.txt)";
       break;
-      case 'None':
+    case 'None':
       break;
   }
   return link;
 
 }
 
-// // TODO: Create a function that returns the license section of README
-// // If there is no license, return an empty string
-// function renderLicenseSection(license) {
-
-// }
+// // // TODO: Create a function that returns the license section of README
+// // // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
 
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  console.log(data);
+  let badgeLic = renderLicenseBadge(data.license);
+  let link = renderLicenseLink(data.license);
   return `# ${data.title}
 
   ## Licensing:
